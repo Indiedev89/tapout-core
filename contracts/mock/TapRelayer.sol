235ded963d8ba980f42598353461336ebc61c333
Indiedev89 <0xdegenerate1990@gmail.com>
2025-07-23 23:19:09 +0800

    added tests

--- contracts/mock/TapRelayer.sol
+++ contracts/mock/TapRelayer.sol
@@ -8,17 +8,17 @@ interface ILastTap {
 }
 
 contract TapRelayer {
-    ILastTap public immutable lastTapInstance; // Use the interface type
+    ILastTap public immutable gameContractInstance; // Use the interface type
     address public owner;
 
     event Approved(address indexed token, address indexed spender, uint256 amount);
     event TappedViaRelay(address indexed relayer);
 
-    constructor(address _lastTapAddress) {
+    constructor(address _gameContract) {
         // When casting to an interface, you typically don't need the 'payable' cast on the address
         // unless the interface functions themselves are payable and you're trying to send value.
         // Since ILastTap.tap() is not payable, this direct cast is fine.
-        lastTapInstance = ILastTap(_lastTapAddress);
+        gameContractInstance = ILastTap(_gameContract);
         owner = msg.sender;
     }
 
@@ -29,15 +29,15 @@ contract TapRelayer {
 
     function approveLastTap(address _tokenAddress, uint256 _amount) external onlyOwner {
         IERC20 token = IERC20(_tokenAddress);
-        // address(lastTapInstance) correctly gets the address of the game contract
-        token.approve(address(lastTapInstance), _amount);
-        emit Approved(_tokenAddress, address(lastTapInstance), _amount);
+        // address(gameContractInstance) correctly gets the address of the game contract
+        token.approve(address(gameContractInstance), _amount);
+        emit Approved(_tokenAddress, address(gameContractInstance), _amount);
     }
 
     function relayTap() external onlyOwner {
         // Ensure this contract (the relayer) has enough tokens and has approved LastTap
         // The actual token transfer will be initiated by LastTap from this contract's address
-        lastTapInstance.tap();
+        gameContractInstance.tap();
         emit TappedViaRelay(address(this));
     }
 
